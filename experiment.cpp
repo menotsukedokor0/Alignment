7aa8f4af9445dce17363dfaa534f63778f105251
wataru <wataru.kuba@gmail.com>
2016-07-25 17:36:52 +0900

    experiment

--- experiment.cpp
+++ experiment.cpp
@@ -2,7 +2,35 @@
 #include "alignment.h"
 #include <fstream>
 #include <typeinfo>
+#include <time.h>
+#include <stdlib.h>
 using namespace std;
+
+void generate_random_sequence(int N, string &S1, string &S2)
+{
+  srand((unsigned int) time(NULL) );
+  for(int i = 0; i < N; i++){
+    int tmp = rand() % 4; 
+    switch( tmp )
+      {
+      case 0: S1 += 'A'; break;
+      case 1: S1 += 'T'; break;
+      case 2: S1 += 'G'; break;
+      case 3: S1 += 'C'; break;
+      default : cerr << "err" << endl;
+      }
+    tmp = rand() % 4;
+    switch( tmp )
+      {
+      case 0: S2 += 'A'; break;
+      case 1: S2 += 'T'; break;
+      case 2: S2 += 'G'; break;
+      case 3: S2 += 'C'; break;
+      default : cerr << "err" << endl;
+      }
+  }
+  
+}
 
 int main(int argc, char* argv[])
 {
@@ -20,13 +48,16 @@ int main(int argc, char* argv[])
   string S1;
   string S2;
 
-  ifstream ifs("input.txt");
-  ifs >> S1;
-  ifs >> S2;
+  ifstream ifs("experiment_input.txt");
   ifs >> m;
   ifs >> x;
   ifs >> o;
   ifs >> e;
+
+  int N;
+  ifs >> N;
+  generate_random_sequence(N, S1, S2);
+
 
   SW_linear sl(S1, S2, m, x, o);
   SW_affine sa(S1, S2, m, x, o, e);
@@ -69,29 +100,38 @@ int main(int argc, char* argv[])
 	}
     }	
    //nコメントアウトした部分でオブジェクトを定義してやると何故か実行時ポリモーフィズムが実現できなくなる。typeid()によるとaln のさすオブジェクトの方が代入後もAlignmentクラスであるこという様子を見ている限り正しく代入できていないようだ。このままのプログラムでいくならif文連発で書いてgoto文を回避できるのでは。
-  cout << "Test sequeces:" << endl;
+  /*cout << "Test sequeces:" << endl;
   cout << S1 << endl;
-  cout << S2 << endl;
+  cout << S2 << endl; 
+  */
   cout << "Alignment Type:" <<
   cout << typeid(*aln).name() << endl;
   string A1, A2;
   aln->calculate_DP_matrix();
-  aln->show(); //DP行列とTRACE行列の確認
+  //aln->show(); //DP行列とTRACE行列の確認
   aln->traceback(A1, A2);
-  for(int i = 0; i < A1.siZe(); ++i)
+  /*
+  for(int i = 0; i < A1.size(); ++i)
     cout << "_";
   cout << endl;
-
   cout << A1 << endl;
   cout << A2 << endl;
+  */
+  int count = 0;
   for(int i = 0; i < A1.size(); i++)
     {
       if(A1[i] == A2[i])
-	cout << '*';
-      else
-	cout << " ";
+	{
+	  //cout << '*';
+	  count ++;
+	}
+      else 
+	{
+	//cout << " ";
+	}
     }
   cout << endl;
-
+  cout << "exact matching rate:";
+  cout << (double)count / N << endl;
   return 0;
 }
