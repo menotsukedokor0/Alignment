46eb7349efb935a3ff76c340148117af413876bc
Wataru Kuba <wataru.kuba@gmail.com>
2016-07-27 18:52:56 +0900

    add comments

--- alignment.h
+++ alignment.h
@@ -14,15 +14,15 @@ class Alignment{
   int o;
  public:
   Alignment(const string &, const string &, const int m1, const int x1, const int o1);
-  virtual void calculate_DP_matrix() = 0;
-  virtual void traceback(string&, string&) = 0;
-  virtual void max(const int, const int) = 0;
+  virtual void calculate_DP_matrix() = 0; //DP行列の計算
+  virtual void traceback(string&, string&) = 0; // トレースバックの計算
+  virtual void max(const int, const int) = 0; // DP行列計算の途中にあるMAX計算、DP行列への書き込みと同時にトレース行列に記録する。
   virtual void show() = 0; //デバック用関数
 };
 
 class NW_linear: public Alignment{
  protected:
-  vector < vector <int> > DP, TRACE;
+  vector < vector <int> > DP, TRACE; //DP行列とトレース行列
  public:
   NW_linear(const string&, const string&, const int m1, const int x1, const int o1);
   void calculate_DP_matrix();
@@ -33,8 +33,8 @@ class NW_linear: public Alignment{
 
 class NW_affine: public Alignment{
  protected:
-  int e;
-  vector < vector <vector <int> > > DP, TRACE;
+  int e; // アフィンクラスにのみアフィンギャプコストのパラメータが存在
+  vector < vector <vector <int> > > DP, TRACE; //DP行列、トレース行列ともに３つのテーブルを用いる.
  public:
   NW_affine(const string&, const string&, const int m1, const int x1, const int o1, const int e1);
   void calculate_DP_matrix();
